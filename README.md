27bebdeb928fd8fd61e9bedb4501121055d2ab29
jcarlossc <josecscosta@fac.pe.senac.br>
2025-04-04 15:26:52 -0300

    README, PHPDocs e LICENSE adicionados.

---
+++ README.md
@@ -0,0 +1,26 @@
+# ESTUDO SOBRE REGEX EM LINGUAGEM DE PROGRAMAÇÃO PHP.
+
+## DESCRIÇÃO:
+Este é um projeto simples que implementa a classe ClasseRegex e seus métodos para validação de strings.
+
+## FERRAMENTAS UTILIZADAS:
+* Linguagem de programação PHP.
+* Composer(gerenciador de dependências PHP)
+* Stack Xampp(Servidor Apacha).
+* Git/GitHub
+* Visual studio code.
+* Windows 10.
+
+## MODO DE UTILIZAR:
+* Instalar o gerenciador de dependências do Php: Composer. Link: https://getcomposer.org/download/
+* Clonar o repositório para o diretório 'C:\xampp\htdocs\', que é o diretório padrão do Xampp. No meu caso 'C:\xampp\htdocs\projetos\', porque prefiro criar um diretório dentro de 'htdocs\'. 
+* Entrar no diretório do projeto: ```cd regex-php```.
+* Na raiz do projeto executar o comando ```composer install``` para instalar os arquivos do diretório 'vendor'.
+* Instalar/executar Stack XAMPP: acessar a raiz da Stack e executar o arquivo ```xampp-control.exe```, após isso, inicializar o servidor Apache. Também é possível usar os servidores Nginx, Wamp ou, no caso do sistema operacional linux, o Lamp.
+* Em um navegador de sua preferência use ```localhost/regex-php/index.php```. No meu caso ```localhost/projetos/regex-php/index.php```.
+
+## Contribuição:
+Se quiser contribuir para este projeto, fique à vontade para enviar um pull request ou relatar problemas na seção de issues.
+
+## Licença:
+Este projeto é licenciado sob a Licença MIT.
--- src/classeregex/ClasseRegex.php
+++ src/classeregex/ClasseRegex.php
@@ -2,10 +2,21 @@
 
 namespace App\classeregex;
 
+/**
+ * A classe ClasseRegex implementa métodos para
+ * validação de strings.
+ *
+ */
 class ClasseRegex {
     public function __construct() {
     }
 
+    /**
+     * Método que valida string do tipo data.
+     * 
+     * @param mixed $data
+     * @return void
+     */
     public function regexData($data) {
         $padrao = "/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/";
         if(preg_match($padrao, $data)){
@@ -15,6 +26,12 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo hora.
+     * 
+     * @param mixed $hora
+     * @return void
+     */
     public function regexHora($hora) {
         $padrao = "/^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/";
         if(preg_match($padrao, $hora)){
@@ -24,6 +41,12 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo cpf.
+     * 
+     * @param mixed $cpf
+     * @return void
+     */
     public function regexCpf($cpf) {
         $padrao = "/^\d{3}\.\d{3}\.\d{3}-\d{2}$/";
         if(preg_match($padrao, $cpf)){
@@ -33,6 +56,12 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo ip.
+     * 
+     * @param mixed $ip
+     * @return void
+     */
     public function regexIp($ip) {
         $padrao = "/^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/";
         if(preg_match($padrao, $ip)){
@@ -42,6 +71,12 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo email.
+     * 
+     * @param mixed $email
+     * @return void
+     */
     public function regexEmail($email) {
         $padrao = "/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/";
         if(preg_match($padrao, $email)){
@@ -51,6 +86,12 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo números.
+     * 
+     * @param mixed $numeros
+     * @return void
+     */
     public function regexNumeros($numeros) {
         $padrao = "/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/";
         if(preg_match($padrao, $numeros)){
@@ -60,6 +101,11 @@ class ClasseRegex {
         }
     }
 
+    /**
+     * Método que valida string do tipo nomes acentuados.
+     * @param mixed $nome
+     * @return void
+     */
     public function regexNomeCaracteresAcento($nome) {
         $padrao = "/^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/u";
         if(preg_match($padrao, $nome)){
